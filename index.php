98e7d089dc22e96205d59b2946a4b3aaa787b405
khalil57 <66666716+khalil57@users.noreply.github.com>
2020-06-15 17:29:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -17,24 +17,3 @@
  */
 
 include '_bootstrap.php';
-?>
-<html>
-    <head>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
-        <style>
-            body {
-                padding: 3rem;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Gateway Test Merchant Server</h1>
-        <p>This is an sample application to help developers start building mobile applications using the Gateway mobile SDK.</p>
-        <h3>Available APIs</h3>
-        <ul>
-            <li><a href="./session.php">Session API</a></li>
-            <li><a href="./transaction.php">Transaction API</a></li>
-            <li><a href="./3DSecure.php">3DSecure API</a></li>
-        </ul>
-    </body>
-</html>
